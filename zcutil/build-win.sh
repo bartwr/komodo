98ac2fec71f5573d07c2fa5be9914fc5e31951c3
dimxy <dimxy@komodoplatform.com>
2022-04-25 00:50:40 +0500

    add --enable-debug support to build

--- zcutil/build-win.sh
+++ zcutil/build-win.sh
@@ -6,23 +6,20 @@ PREFIX="$(pwd)/depends/$HOST"
 
 set -eu -o pipefail
 
-set -x
-cd "$(dirname "$(readlink -f "$0")")/.."
+UTIL_DIR="$(dirname "$(readlink -f "$0")")"
+BASE_DIR="$(dirname "$(readlink -f "$UTIL_DIR")")"
+PREFIX="$BASE_DIR/depends/$HOST"
 
 # make dependences
 cd depends/ && make HOST=$HOST V=1 NO_QT=1
 cd ../
 
-# note: cclib building now added to src/Makefile.am
-#BUILD CCLIB
-# WD=$PWD
-# cd src/cc
-# echo $PWD
-# ./makecustom
-# cd $WD
+cd $BASE_DIR/depends
+make HOST=$HOST NO_QT=1 "$@"
+cd $BASE_DIR
 
 ./autogen.sh
-CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site CXXFLAGS="-DPTW32_STATIC_LIB -DCURL_STATICLIB -DCURVE_ALT_BN128 -fopenmp -pthread" ./configure --prefix="${PREFIX}" --host=x86_64-w64-mingw32 --enable-static --disable-shared \
+CONFIG_SITE=$BASE_DIR/depends/$HOST/share/config.site CXXFLAGS="-DPTW32_STATIC_LIB -DCURL_STATICLIB -DCURVE_ALT_BN128 -fopenmp -pthread" ./configure --prefix=$PREFIX --host=$HOST --enable-static --disable-shared \
   --with-custom-bin=yes CUSTOM_BIN_NAME=tokel CUSTOM_BRAND_NAME=Tokel \
   CUSTOM_SERVER_ARGS="'-ac_name=TOKEL -ac_supply=100000000 -ac_eras=2 -ac_cbmaturity=1 -ac_reward=100000000,4250000000 -ac_end=80640,0 -ac_decay=0,77700000 -ac_halving=0,525600 -ac_cc=555 -ac_ccenable=236,245,246,247 -ac_adaptivepow=6 -addnode=135.125.204.169 -addnode=192.99.71.125 -nspv_msg=1'" \
   CUSTOM_CLIENT_ARGS='-ac_name=TOKEL'
